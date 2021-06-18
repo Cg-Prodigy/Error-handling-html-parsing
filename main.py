5fd0ddc3181cfb5fd87d9f03d413caf1ef7869c9
Cg-Prodigy <brightmarshall2@gmail.com>
2021-06-18 20:46:29 +0100

    Refinment of the error handling algo

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError,URLError
 from bs4 import BeautifulSoup
-
+# http://www.pythonscraping.com/pages/page1.html
 def errorHandler(url):
     try:
         urlObj=urlopen(url)
     except HTTPError as e:
         return e
     except URLError:
-        return 'Mistyped URL. Server is currently down'
+        return 'Mistyped URL or Server is currently down'
     try:
         bsObj=BeautifulSoup(urlObj.read(), 'html5lib')
-    except AttributeError:
-        return 'Element is not present'
-    return bsObj
+        result=bsObj.body.h2
+    except AttributeError as e:
+        return "{e}. Element doesn't exist".format(e)
+    else: 
+        return result
     
+my_url= 'http://www.pythonscraping.com/pages/page1.html'
+trial=errorHandler(my_url)
+if trial==None:
+    print("Element doesn't exist")
+print(trial)
