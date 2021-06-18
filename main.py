0483d6b8ffede32278fa58189ac4a1732ba1d706
Cg-Prodigy <brightmarshall2@gmail.com>
2021-06-18 20:34:33 +0100

    Error handling algo

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Working with git")
+from urllib.request import urlopen
+from urllib.error import HTTPError,URLError
+from bs4 import BeautifulSoup
+
+def errorHandler(url):
+    try:
+        urlObj=urlopen(url)
+    except HTTPError as e:
+        return e
+    except URLError:
+        return 'Mistyped URL. Server is currently down'
+    try:
+        bsObj=BeautifulSoup(urlObj.read(), 'html5lib')
+    except AttributeError:
+        return 'Element is not present'
+    return bsObj
+    
